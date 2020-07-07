d13484b9aadfddea66bc6a3bf14715c65af162d0
Cvijetin <ivanasvalina15@gmail.com>
2020-07-07 09:16:04 +0200

    inital commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const menuIcon = document.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menu");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("searchbox");
+
+searchIcon.addEventListener('click', function () {
+  if (searchBox.style.top == '72px') {
+    searchBox.style.top = '24px';
+    searchBox.style.pointerEvents = 'none';
+  } else {
+    searchBox.style.top = '72px';
+    searchBox.style.pointerEvents = 'auto';
+  }
+});
+
+menuIcon.addEventListener('click', function () {
+  if (slideoutMenu.style.opacity == "1") {
+    slideoutMenu.style.opacity = '0';
+    slideoutMenu.style.pointerEvents = 'none';
+  } else {
+    slideoutMenu.style.opacity = '1';
+    slideoutMenu.style.pointerEvents = 'auto';
+  }
+});
+
+
+
+// if the image in the window of browser when the page is loaded, show that image
+$(function () {
+	$(document).scroll(function () {
+	  var $nav = $(".navbar");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+	});
+	
+	function showImages(el) {
+    var windowHeight = jQuery( window ).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos ) {
+            $(this).addClass("fadeIn");
+        }
+    });
+}
+
+// if the image in the window of browser when the page is loaded, show that image
+$(document).ready(function(){
+    showImages('.star');
+});
+
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function() {
+    showImages('.star');
+});
+
+
+
+
